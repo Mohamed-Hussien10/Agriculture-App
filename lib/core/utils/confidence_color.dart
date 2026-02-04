0e37196b975a2a538e6f23699c186ffeb993201d
Mohamed-Hussien10 <mohamedhu350@gmail.com>
2026-02-04 11:11:13 +0200

    add app permissions

--- lib/core/utils/confidence_color.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color getConfidenceColor(double confidence) {
-  if (confidence >= 80) return Colors.green;
-  if (confidence >= 60) return Colors.orange;
-  return Colors.red;
-}
